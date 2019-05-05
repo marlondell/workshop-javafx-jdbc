281bbe6d3b93ea764616dc2c00d9659563b035c3
marlondell <marlondellaliberah@outlook.com>
2019-05-05 20:16:27 -0300

    DepartmentList Controller

---
+++ src/gui/DepartmentListController.java
@@ -0,0 +1,55 @@
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import application.Main;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+import model.entities.Department;
+
+public class DepartmentListController implements Initializable{
+
+	//Creating references to the elements of the Tableview
+	
+	@FXML
+	private TableView<Department> tableviewDepartment;
+	
+	@FXML
+	private TableColumn<Department, Integer> tableColumnID;
+	
+	@FXML
+	private TableColumn<Department, String> tableColumnName;
+	
+	@FXML
+	private Button btNew;
+	
+	@FXML
+	public void onBtNewAction() {
+		System.out.println("onBtNewAction");
+	}
+	
+	//We need to create the method initializeNodes to works correctly the columns "massete"
+	//to start the behavior of the columns
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		initializeNodes();
+	}
+
+	private void initializeNodes() {
+		tableColumnID.setCellValueFactory(new PropertyValueFactory<>("id"));
+		tableColumnName.setCellValueFactory(new PropertyValueFactory<>("name"));
+		
+		//The table is smaller than the Main screen. To fix it, we need get the current Scene
+		Stage stage = (Stage) Main.getMainScene().getWindow();
+		tableviewDepartment.prefHeightProperty().bind(stage.heightProperty());
+	}
+	
+	
+
+}
